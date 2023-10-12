9448b9b86b11ae79735ee10cbaf57e1e69244087
gofla1996 <110741@daum.net>
2023-10-12 17:38:59 +0900

    Feat: 마이페이지 포스트 탭 아티스트 탭메뉴 제작(진행중)

--- src/pages/MyPage/Posts/index.jsx
+++ src/pages/MyPage/Posts/index.jsx
@@ -2,13 +2,24 @@ import { useState, useEffect } from "react";
 import * as s from "./style";
 
 const Posts = () => {
-  
+  const [isClicked, setIsClicked] = useState(false);
 
+  // const artists = [뉴진스, 르세라핌, 방탄소년단];
 
 
   return (
     <>
-      <s.Wrapper> 포스트 </s.Wrapper>
+      <s.Wrapper>
+        <s.ArtistsTabWrapper>
+          <s.ArtistsTab
+            // onClick={onClickArtist}
+            isClicked={isClicked}
+            className={isClicked ? 'active' : ''}
+            artistIndex={0}
+          >뉴진스</s.ArtistsTab>
+          <s.ArtistsTab>{ }</s.ArtistsTab>
+        </s.ArtistsTabWrapper>
+      </s.Wrapper>
     
     </>
   )
--- src/pages/MyPage/Posts/style.js
+++ src/pages/MyPage/Posts/style.js
@@ -2,7 +2,51 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
+
   width: 100%;
   height: 100%;
+ 
+  min-width: 1000px;
 
 `;
+
+export const ArtistsTabWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+
+  width: auto;
+  height: 30px;
+  
+  margin: 0 40px;
+
+
+  border: 1px solid red;
+`;
+
+export const ArtistsTab = styled.button`
+  display: flex;
+  align-items: center;
+
+  width: fit-content;
+  height: 100%;
+
+  margin: 0 10px;
+
+  border: none;
+
+  background-color: transparent;
+
+  font-size: 15px;
+  font-weight: bold;
+  color: gray;
+
+  cursor: pointer;
+
+  &.active {
+    color: black;
+  }
+`;
+
