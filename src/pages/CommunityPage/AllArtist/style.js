37ab06aa2d81f18479acd453db6f19c3d194a6ea
iamin127 <iamin127@naver.com>
2023-11-05 23:10:39 +0900

    feat:~~

---
+++ src/pages/CommunityPage/AllArtist/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ContentWrapper = styled.div`
+  width: fit-content;
+  height: fit-content;
+  margin: 40px 20px 40px 20px;
+  padding: 15px;
+  font-size: 25px;
+  font-weight: bold;
+`;
--- src/pages/CommunityPage/index.jsx
+++ src/pages/CommunityPage/index.jsx
@@ -1,1 +1,9 @@
+import Header from '../../components/Header';
 import * as s from './style';
+
+const CommunityPage = () => {
+  return;
+  <Header />;
+};
+
+export default CommunityPage;
