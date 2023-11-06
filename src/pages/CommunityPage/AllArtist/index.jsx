90d535517b86e9d9f6360c6c2e970ce838b1d801
iamin127 <iamin127@naver.com>
2023-11-06 20:59:41 +0900

    feat:에러 해결

--- src/pages/CommunityPage/AllArtist/index.jsx
+++ src/pages/CommunityPage/AllArtist/index.jsx
@@ -1,7 +1,25 @@
+import React from 'react';
+import * as s from './style';
 import Header from '../../../components/Header';
+import CardsSlider from '../../../components/CardSlider';
+
+import AllArtists from '../../../Temp/mainpage/AllArtists';
+
+import ArtistCard from '../../../components/ArtistCard';
 
 const AllArtist = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+
+      <s.PageLabel>{AllArtists[0].enterComp}</s.PageLabel>
+      <CardsSlider>
+        {AllArtists.map((item) => (
+          <ArtistCard fileUrl={item.fileUrl} artistName={item.artistName} />
+        ))}
+      </CardsSlider>
+    </>
+  );
 };
 
 export default AllArtist;
--- src/pages/CommunityPage/AllArtist/style.js
+++ src/pages/CommunityPage/AllArtist/style.js
@@ -8,3 +8,12 @@ export const ContentWrapper = styled.div`
   font-size: 25px;
   font-weight: bold;
 `;
+
+export const PageLabel = styled.div`
+  width: fit-content;
+  height: fit-content;
+  margin: 40px 20px 40px 20px;
+  padding: 15px;
+  font-size: 25px;
+  font-weight: bold;
+`;
