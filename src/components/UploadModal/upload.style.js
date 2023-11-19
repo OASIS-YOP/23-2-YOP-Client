2d4b94b4a63c4e3957b454903b8b6d0def7af3b3
iamin127 <iamin127@naver.com>
2023-11-20 00:09:43 +0900

    모달임시커밋

--- src/components/UploadModal/upload.style.js
+++ src/components/UploadModal/upload.style.js
@@ -57,3 +57,46 @@ export const CollectionCard = styled.div`
     height: 100%;
   }
 `;
+
+export const Label = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 100%;
+  background-color: gray;
+  color: white;
+  border-radius: 20px;
+`;
+
+export const PolaroidWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  width: 100%;
+  height: fit-content;
+  border-radius: 20px;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px auto;
+  /* margin-top: 50px;
+  margin-left: 50px; */
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const PolaroidCard = styled.div`
+  width: 160px;
+  height: 160px;
+  border-radius: 10px;
+  border: 1px solid 3F70FF;
+  margin: 0 15px 40px 15px;
+  overflow: hidden;
+  img {
+    width: 120%;
+    height: 160%;
+  }
+`;
